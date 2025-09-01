485e661a40d17bfd63cb2645d1ebcc535d4ffaa2
sparrow <sparrow@wsl.me>
2025-09-01 22:28:59 +0800

    Mon Sep  1 22:28:58 CST 2025

--- .make/depend.dir/libast.mk
+++ .make/depend.dir/libast.mk
@@ -1,9 +1,9 @@
-DEPEND_TARGETS += libast.build
+DEPEND_TARGETS += libast.recursive
 ECHO_TARGETS += libast.echo
-libast.build:
+libast.recursive:
 	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libes/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/.make/super
 	@echo SUPER_MAKE_CONFIG_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libes/.make >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/.make/super
-	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/ && make && make install
+	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/ && make recursive && make install
 	-rm /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/.make/super
 libast.echo:
 	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libes/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/.make/super
