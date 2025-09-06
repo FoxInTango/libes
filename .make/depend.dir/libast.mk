872dfe36082bfcee3538cf89be92a5ff8863097a
sparrow <sparrow@wsl.me>
2025-09-07 00:09:31 +0800

    Sun Sep  7 00:09:30 CST 2025

--- .make/depend.dir/libast.mk
+++ .make/depend.dir/libast.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libast.recursive
 ECHO_TARGETS += libast.echo
 libast.recursive:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libes/               >> /home/lidali/alpine/libraries/libast/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libes/.make >> /home/lidali/alpine/libraries/libast/.make/super
-	cd /home/lidali/alpine/libraries/libast/ && make recursive && make install
-	-rm /home/lidali/alpine/libraries/libast/.make/super
+	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libes/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libes/.make >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/.make/super
+	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/ && make recursive && make install
+	-rm /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/.make/super
 libast.echo:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libes/               >> /home/lidali/alpine/libraries/libast/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libes/.make >> /home/lidali/alpine/libraries/libast/.make/super
-	cd /home/lidali/alpine/libraries/libast/ && make echo
-	-rm /home/lidali/alpine/libraries/libast/.make/super
+	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libes/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libes/.make >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/.make/super
+	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/ && make echo
+	-rm /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/.make/super
