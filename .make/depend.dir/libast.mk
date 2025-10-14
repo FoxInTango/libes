3bb1e4eeeb385dece24bf04582c00c66a8987aea
李诗军 <lidali@AllinoneBook.local>
2025-10-14 15:57:29 +0800

    2025年10月14日 星期二 15时57分29秒 CST

--- .make/depend.dir/libast.mk
+++ .make/depend.dir/libast.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libast.recursive
 ECHO_TARGETS += libast.echo
 libast.recursive:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libes/               >> /volumes/llama/home/alpine/libraries/libast/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libes/.make >> /volumes/llama/home/alpine/libraries/libast/.make/super
-	cd /volumes/llama/home/alpine/libraries/libast/ && make recursive && make install
-	-rm /volumes/llama/home/alpine/libraries/libast/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/libraries/libes/               >> /Users/lidali/alpine/libraries/libast/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/libraries/libes/.make >> /Users/lidali/alpine/libraries/libast/.make/super
+	cd /Users/lidali/alpine/libraries/libast/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make recursive && /Applications/Xcode.app/Contents/Developer/usr/bin/make install
+	-rm /Users/lidali/alpine/libraries/libast/.make/super
 libast.echo:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libes/               >> /volumes/llama/home/alpine/libraries/libast/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libes/.make >> /volumes/llama/home/alpine/libraries/libast/.make/super
-	cd /volumes/llama/home/alpine/libraries/libast/ && make echo
-	-rm /volumes/llama/home/alpine/libraries/libast/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/libraries/libes/               >> /Users/lidali/alpine/libraries/libast/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/libraries/libes/.make >> /Users/lidali/alpine/libraries/libast/.make/super
+	cd /Users/lidali/alpine/libraries/libast/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make echo
+	-rm /Users/lidali/alpine/libraries/libast/.make/super
